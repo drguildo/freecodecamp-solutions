835823f7b4ae586c09a571f4e723e19355d799f1
Simon Morgan <sjm@sjm.io>
2017-04-28 12:01:32 +0100

    Allow incrementing and decrementing of break and session length

---
+++ pomodoro/script.js
@@ -0,0 +1,89 @@
+// jshint esversion: 6
+
+/*
+NOTES
+-----
+Use `requestAnimationFrame` for timer?
+Probably `setInterval` is better.
+*/
+
+function ready(fn) {
+  if (document.readyState != 'loading') {
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+function id(id) {
+  return document.getElementById(id);
+}
+
+function getBreakLength() {
+  return Number(id("break-length").innerText);
+}
+
+function setBreakLength(minutes) {
+  if (minutes < 0) {
+    return;
+  }
+  id("break-length").innerText = minutes;
+}
+
+function getSessionLength() {
+  return Number(id("session-length").innerText);
+}
+
+function setSessionLength(minutes) {
+  if (minutes < 0) {
+    return;
+  }
+  id("session-length").innerText = minutes;
+}
+
+function decrementBreakLengthClicked() {
+  setBreakLength(getBreakLength() - 1);
+}
+
+function incrementBreakLengthClicked() {
+  setBreakLength(getBreakLength() + 1);
+}
+
+function decrementSessionLengthClicked() {
+  setSessionLength(getSessionLength() - 1);
+}
+
+function incrementSessionLengthClicked() {
+  setSessionLength(getSessionLength() + 1);
+}
+
+function startTimer() {}
+
+function pauseTimer() {}
+
+function updateMeter(percentage) {
+  if (percentage < 0) {
+    return;
+  }
+
+  let meterFill = id("progress-fill");
+  meterFill.style.top = percentage + "%";
+}
+
+function startClicked() {
+
+}
+
+function setupEventListeners() {
+  console.log("configuring event listeners");
+
+  id("decrement-break-length").addEventListener("click", decrementBreakLengthClicked);
+  id("increment-break-length").addEventListener("click", incrementBreakLengthClicked);
+
+  id("decrement-session-length").addEventListener("click", decrementSessionLengthClicked);
+  id("increment-session-length").addEventListener("click", incrementSessionLengthClicked);
+
+  id("start-button").addEventListener("click", startClicked);
+}
+
+ready(setupEventListeners);
