8e555be1214655bd90c1b9a4b3c7da7a965803b2
Simon Morgan <sjm@sjm.io>
2017-07-19 16:01:24 +0100

    Change cell contents on click event

--- tictactoe/js/script.js
+++ tictactoe/js/script.js
@@ -1,7 +1,17 @@
 function ready(fn) {
-  if (document.readyState != "loading") {
+  if (document.readyState !== "loading") {
     fn();
   } else {
     document.addEventListener("DOMContentLoaded", fn);
   }
 }
+
+ready(() => {
+  let cells = document.querySelectorAll(".cell");
+  cells.forEach(function(el) {
+    el.addEventListener("click", (event) => {
+      console.log(event.target);
+      event.target.textContent = "X";
+    });
+  });
+});
