4c5de2ea270059edfabe7573ce46ac92b71d9269
Simon Morgan <sjm@sjm.io>
2017-07-31 12:32:52 +0100

    Allow user to play a game versus the computer by clicking on the grid

--- tictactoe/js/script.js
+++ tictactoe/js/script.js
@@ -1,3 +1,40 @@
+let gameState = {
+  // The two players are represented as 1 and -1. This is because it's
+  // how the underlying minimax algorithm treats them and so does away
+  // with the need to be constantly converting between the two
+  // representations.
+  board: []
+};
+
+function getCellValue(row, col) {
+  return ~~document.querySelector(`#cell-${row}-${col}`).textContent;
+}
+
+function setCellValue(row, col, val) {
+  document.querySelector(`#cell-${row}-${col}`).textContent = val;
+}
+
+// Convert the game state stored in the HTML to a two-dimensional array
+// that can be used by our minimax algorithm.
+function domToArray() {
+  let arr = [];
+  for (let row = 0; row < 3; row++) {
+    arr.push([]);
+    for (let col = 0; col < 3; col++) {
+      arr[row].push(getCellValue(row, col));
+    }
+  }
+  return arr;
+}
+
+function cellClickHandler(event) {
+  event.target.textContent = 1;
+  gameState.board = domToArray();
+  let bestOpponentMove = minimax(-1, gameState.board).move;
+  console.log(bestOpponentMove);
+  setCellValue(...bestOpponentMove, -1);
+};
+
 function ready(fn) {
   if (document.readyState !== "loading") {
     fn();
